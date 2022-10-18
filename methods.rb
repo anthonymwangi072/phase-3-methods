5ecd769069ef8399428cbe19694a78990e1cbffd
Anthony Mwaniki <anthonymwangi072@gmail.com>
2022-10-18 15:58:29 +0300

    completed lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,52 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+greet  "Jimmy"
+
+
+
+
+def greet_with_default(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+add(3, 4)
+
+
+
+def halve (number)
+    if number.class != Integer
+        return nil
+    end
+    number/2
+end
+halve ("two")
+halve (2)
+
+
+
+
+
+
+
+
